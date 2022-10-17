725a91cb8a6aa66f84b756dd7520678d0750fc01
cjnour <charbel.nour9@gmail.com>
2022-10-16 21:38:20 -0400

    Fixed footer, added simple navbar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import MuiMenu from "./Components/MuiMenu";
 
 const App = () => {
   return (
     <div className="App">
       <div className="container1">
-        <h1>RateMyLandlord</h1>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div>
+            <h1>RateMyLandlord</h1>
+          </div>
+          <div style={{ flex: "1" }}>
+            <MuiMenu />
+          </div>
+        </div>
       </div>
       <div className="container2">
         <h1>the good stuff</h1>
