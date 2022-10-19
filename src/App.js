cd7a0da7f5c27d222392e50f8cb4e70a14940448
cjnour <charbel.nour9@gmail.com>
2022-10-18 20:36:45 -0400

    Updated FavIcon and fixed felxbox on Navbar.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ const App = () => {
   return (
     <div className="App">
       <div className="container1">
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ flex: "1", marginLeft: "10px" }}>
             <h1>RateMyLandlord</h1>
           </div>
-          <div style={{ flex: "1" }}>
+          <div style={{ flex: "5" }}></div>
+          <div style={{ flex: "1", marginRight: "10px" }}>
             <MuiMenu />
           </div>
         </div>
