3c36d88c0f47f39a532ccbc74f8af0b17e51f948
cjnour <charbel.nour9@gmail.com>
2022-11-06 17:05:45 -0500

    Installed Reacter Router Dom.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import "./App.css";
 import MuiContainer from "./Components/MuiContainer";
 import MuiMenu from "./Components/MuiMenu";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="container1">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ flex: "1", marginLeft: "10px" }}>
-            <h1>RateMyLandlord</h1>
-          </div>
-          <div style={{ flex: "5" }}></div>
-          <div style={{ flex: "1", marginRight: "10px" }}>
-            <MuiMenu />
+      <Router>
+        <div className="container1">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <div style={{ flex: "1", marginLeft: "10px" }}>
+              <h1>RateMyLandlord</h1>
+            </div>
+            <div style={{ flex: "5" }}></div>
+            <div style={{ flex: "1", marginRight: "10px" }}>
+              <MuiMenu />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container2">
-        <MuiContainer />
-      </div>
-      <div className="container3">
-        <p>
-          made by{" "}
-          <a href="https://chese.ca" target="_blank" rel="noreferrer">
-            CJ Nour
-          </a>
-        </p>
-      </div>
+        <div className="container2">
+          <MuiContainer />
+        </div>
+        <div className="container3">
+          <p>
+            made by{" "}
+            <a href="https://chese.ca" target="_blank" rel="noreferrer">
+              CJ Nour
+            </a>
+          </p>
+        </div>
+      </Router>
     </div>
   );
 };
